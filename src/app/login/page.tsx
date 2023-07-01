6ce73693981e3e328e310d394a275db3dc5ce0e2
Marcus Davanço <md.daves@gmail.com>
2023-07-01 07:14:50 -0300

    style: login linting

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 import logo from '../../../public/logo.svg'
 import hero from '../../../public/hero_image.jpg'
@@ -9,32 +9,56 @@ import guestIcon from '../../../public/guest-icon.svg'
 
 export default function Login() {
   return (
-    <main className="p-5 flex min-h-full" >
-      <section className='hidden md:block w-[598px] relative '>
-        <Image height="58" width="232" alt="" src={logo} className="absolute top-1/2 left-1/2 translate-x-[-50%] z-10" />
+    <main className="p-5 flex min-h-full">
+      <section className="hidden md:block w-[598px] relative ">
+        <Image
+          height="58"
+          width="232"
+          alt=""
+          src={logo}
+          className="absolute top-1/2 left-1/2 translate-x-[-50%] z-10"
+        />
         <Image alt="" src={hero} quality={100} className="rounded-md" fill />
       </section>
       <section className="w-[350px] m-auto ">
         <header className=" mb-10">
           <h2 className="text-2xl font-bold leading-short">Boas vindas!</h2>
-          <p className="text-gray-200" >Faça seu login ou acesse como visitante.</p>
+          <p className="text-gray-200">
+            Faça seu login ou acesse como visitante.
+          </p>
         </header>
         <div className="flex flex-col gap-4">
           <button className="flex justify-start items-center bg-gray-600 hover:bg-gray-500 px-6 py-5 rounded-[8px] text-gray-200 font-bold transition-all duration-300 ease-out hover:ease-in">
-            <Image height="32" width="32" alt="" src={githubIcon} className="mr-5" />
+            <Image
+              height="32"
+              width="32"
+              alt=""
+              src={githubIcon}
+              className="mr-5"
+            />
             Entrar com Google
           </button>
           <button className="flex justify-start items-center bg-gray-600 hover:bg-gray-500 px-6 py-5 rounded-[8px] text-gray-200 font-bold transition-all duration-300 ease-out hover:ease-in">
-            <Image height="32" width="32" alt="" src={googleIcon} className="mr-5" />
+            <Image
+              height="32"
+              width="32"
+              alt=""
+              src={googleIcon}
+              className="mr-5"
+            />
             Entrar com GitHub
           </button>
           <button className="flex justify-start items-center bg-gray-600 hover:bg-gray-500 px-6 py-5 rounded-[8px] text-gray-200 font-bold transition-all duration-300 ease-out hover:ease-in">
-            <Image height="32" width="32" alt="" src={guestIcon} className="mr-5" />
+            <Image
+              height="32"
+              width="32"
+              alt=""
+              src={guestIcon}
+              className="mr-5"
+            />
             Acessar como visitante
           </button>
         </div>
-
-
       </section>
     </main>
   )
