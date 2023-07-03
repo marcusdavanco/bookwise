0bcdeca544b6e6bf9aaaf2b47d07243609f28fe9
Marcus Davanço <md.daves@gmail.com>
2023-07-03 10:55:06 -0300

    wip: home page styling

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,21 +6,23 @@ export default function Home() {
   return (
     <main className="p-5 flex min-h-full">
       <Sidebar />
-      <section>
-        <div>
-          <LineChart className="h-8 w-8" />
-          <h2>Início</h2>
-        </div>
-        <span>Avaliações mais recentes</span>
-        <div>
-          <BookReviewCard />
-          <BookReviewCard />
-          <BookReviewCard />
-        </div>
-      </section>
-      <section>
+      <div className="flex gap-16 place-content-center">
+        <section>
+          <div className="flex gap-3 mb-10">
+            <LineChart className="h-8 w-8 text-green-100" />
+            <h2 className="text-2xl font-bold">Início</h2>
+          </div>
+          <div className="flex flex-col gap-3">
+            <span className="mb-1 text-sm">Avaliações mais recentes</span>
+            <BookReviewCard />
+            <BookReviewCard />
+            <BookReviewCard />
+          </div>
+        </section>
+        <section>
 
-      </section>
+        </section>
+      </div>
     </main>
   )
 }
