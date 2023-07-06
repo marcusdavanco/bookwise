f19e1f935b0ad281e6609f600a2b139230fe6ca3
Marcus Davanço <md.daves@gmail.com>
2023-07-06 17:39:34 -0300

    feat: github provider authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
 
 export const authOption = {
   secret: process.env.NEXTAUTH_SECRET ?? '',
@@ -9,6 +10,10 @@ export const authOption = {
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+    })
   ],
   pages: {
     signIn: "/login",
