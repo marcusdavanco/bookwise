7c3706b6ee65b4cc53cf4b84a9db1d91d8393762
Marcus Davanço <md.daves@gmail.com>
2023-07-31 20:57:06 -0300

    wip: persisting user in the database

--- src/components/signin-button.tsx
+++ src/components/signin-button.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { api } from '@/lib/axios'
+import { AxiosError } from 'axios'
 import { signIn } from 'next-auth/react'
 import Image from 'next/image'
 
@@ -9,13 +11,27 @@ interface SignInButtonProps {
 }
 
 export function SignInButton({ imageUrl, providerName }: SignInButtonProps) {
+  async function handleRegister() {
+    try {
+      // await signIn(providerName, {
+      //   callbackUrl: `${window.location.hostname}/home`,
+      // })
+      await api.post('/users', {
+        // TODO - There's no data to be sent here as this is not a form...
+      })
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response?.data.message)
+        return
+      }
+
+      console.error(err)
+    }
+  }
+
   return (
     <button
-      onClick={async () =>
-        await signIn(providerName, {
-          callbackUrl: `${window.location.hostname}/home`,
-        })
-      }
+      onClick={handleRegister}
       className="flex justify-start items-center bg-gray-600 hover:bg-gray-500 px-6 py-5 rounded-[8px] text-gray-200 font-bold transition-all duration-300 ease-out hover:ease-in"
     >
       <Image height="32" width="32" alt="" src={imageUrl} className="mr-5" />
