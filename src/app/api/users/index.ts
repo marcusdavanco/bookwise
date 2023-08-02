6a8c0c8b4b8fc3b45f635fdaf48deaecbb74155c
Marcus Davanço <md.daves@gmail.com>
2023-08-02 16:40:19 -0300

    wip: custom adapter

--- src/app/api/users/index.ts
+++ src/app/api/users/index.ts
@@ -6,8 +6,11 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { setCookie } from 'nookies'
 import { prisma } from '@/lib/prisma'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST'){
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
     return res.status(405).end()
   }
 
@@ -15,11 +18,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const userExists = await prisma.user.findUnique({
     where: {
-      name
-    }
+      name,
+    },
   })
 
-  if(userExists) {
+  if (userExists) {
     return res.status(400).json({
       message: 'Username already taken.',
     })
@@ -27,10 +30,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const user = await prisma.user.create({
     data: {
-      name 
-    }
+      name,
+    },
   })
-  
+
   setCookie({ res }, '@bookwise:userId', user.id, {
     maxAge: 60 * 60 * 24 * 7, // 7 days
     path: '/',
