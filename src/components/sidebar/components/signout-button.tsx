2cd71e7a431f49a9f1cae82e79f7d9ba26de9a0d
Marcus Davanço <md.daves@gmail.com>
2023-10-21 14:13:55 -0300

    feat: signout redirection

--- src/components/sidebar/components/signout-button.tsx
+++ src/components/sidebar/components/signout-button.tsx
@@ -1,10 +1,16 @@
 'use client'
 import { LogOutIcon } from 'lucide-react'
 import { signOut } from 'next-auth/react'
+import { redirect } from 'next/navigation'
 
 export function SignOutButton() {
+  const handleSignout = () => {
+    signOut()
+    redirect('/login')
+  }
+
   return (
-    <button onClick={() => signOut()}>
+    <button onClick={handleSignout}>
       <LogOutIcon className="h-7 w-7" color="#f75a68" />
     </button>
   )
