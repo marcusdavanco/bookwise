0cfd8149fa3b042c84b2c0b0b697e4b48959d47f
Marcus Davanço <md.daves@gmail.com>
2023-10-21 13:52:21 -0300

    fix: response adjustment

--- src/app/api/users/[user]/route.ts
+++ src/app/api/users/[user]/route.ts
@@ -1,14 +1,25 @@
 import { prisma } from '@/lib/prisma'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
   const id = req.nextUrl.pathname.split('/').pop()
 
-  const user = await prisma.user.findFirstOrThrow({
+  if (!id) {
+    return NextResponse.json({ message: 'id is required.' }, { status: 400 })
+  }
+
+  const user = await prisma.user.findFirst({
     where: {
       id,
     },
   })
 
-  return user
+  if (!user) {
+    return NextResponse.json(
+      { message: 'resource not found.' },
+      { status: 404 },
+    )
+  }
+
+  return NextResponse.json({ user }, { status: 200 })
 }
