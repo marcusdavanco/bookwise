090ea0d210b9f35bf8958f06bfcb16e9ebb11d8a
Marcus Davanço <md.daves@gmail.com>
2023-10-24 13:23:15 -0300

    feat: categories query

---
+++ src/hooks/queries/categories.ts
@@ -0,0 +1,33 @@
+import { api } from '@/lib/axios'
+import { UseQueryOptions, useQuery } from '@tanstack/react-query'
+
+interface Category {
+  id: string
+  name: string
+}
+
+// QueryKey
+export type CategoriesQueryKey = ['categories']
+
+// queryFn
+async function fetchCategories() {
+  const { data: categories } = await api.get('/categories')
+
+  return categories
+}
+
+// Hook
+export const useBooks = <Tdata = { categories: Category[] }>(
+  options?: UseQueryOptions<
+    { books: Category[] },
+    unknown,
+    Tdata,
+    CategoriesQueryKey
+  >,
+) => {
+  return useQuery({
+    queryKey: ['categories'],
+    queryFn: fetchCategories,
+    ...options,
+  })
+}
