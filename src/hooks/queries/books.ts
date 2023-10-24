2140181f6b20762b1954459fb2435b4ba79e4602
Marcus Davanço <md.daves@gmail.com>
2023-10-23 21:06:31 -0300

    feat: book queries

---
+++ src/hooks/queries/books.ts
@@ -0,0 +1,88 @@
+import { api } from '@/lib/axios'
+import {
+  QueryFunctionContext,
+  UseQueryOptions,
+  useQuery,
+} from '@tanstack/react-query'
+
+interface Book {
+  id: string
+  name: string
+  author: string
+  summary: string
+  cover_url: string
+  total_pages: number
+  created_at: Date
+}
+
+// QueryKey
+export type BooksQueryKey = ['books']
+export type BooksByIdQueryKey = ['books', string]
+export type BooksByCategoryIdQueryKey = ['books', string]
+
+// queryFn
+async function fetchBooks() {
+  const { data: books } = await api.get('/books')
+
+  return books
+}
+
+async function fetchBooksByCategory({
+  queryKey,
+}: QueryFunctionContext<BooksByCategoryIdQueryKey>) {
+  const [, id] = queryKey
+
+  const { data: books } = await api.get(`/books/category/${id}`)
+
+  return books
+}
+
+async function getBookById({
+  queryKey,
+}: QueryFunctionContext<BooksByIdQueryKey>) {
+  const [, id] = queryKey
+
+  const { data: book } = await api.get(`/books/${id}`)
+
+  return book
+}
+
+// Hook
+export const useBooks = <Tdata = { books: Book[] }>(
+  options?: UseQueryOptions<{ books: Book[] }, unknown, Tdata, BooksQueryKey>,
+) => {
+  return useQuery({
+    queryKey: ['books'],
+    queryFn: fetchBooks,
+    ...options,
+  })
+}
+
+export const useBooksById = <Book>(
+  id: string,
+  options?: UseQueryOptions<Book, unknown, Book, BooksByIdQueryKey>,
+) => {
+  return useQuery<Book, unknown, Book, BooksByIdQueryKey>({
+    queryKey: ['books', id],
+    queryFn: getBookById,
+    ...options,
+    enabled: !!id,
+  })
+}
+
+export const useBooksByCategory = <Tdata = { books: Book[] }>(
+  id: string,
+  options?: UseQueryOptions<
+    { books: Book[] },
+    unknown,
+    Tdata,
+    BooksByCategoryIdQueryKey
+  >,
+) => {
+  return useQuery({
+    queryKey: ['books', id],
+    queryFn: fetchBooksByCategory,
+    ...options,
+    enabled: !!id,
+  })
+}
