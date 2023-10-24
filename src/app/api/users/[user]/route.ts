118fbcdce53f200d8d2cd5c06846c463acdc3a5d
Marcus Davanço <md.daves@gmail.com>
2023-10-24 13:49:15 -0300

    feat: user api route

---
+++ src/app/api/users/[user]/route.ts
@@ -0,0 +1,25 @@
+import { prisma } from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  const id = req.nextUrl.pathname.split('/').pop()
+
+  if (!id) {
+    return NextResponse.json({ message: 'id is required.' }, { status: 400 })
+  }
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id,
+    },
+  })
+
+  if (!user) {
+    return NextResponse.json(
+      { message: 'resource not found.' },
+      { status: 404 },
+    )
+  }
+
+  return NextResponse.json({ user }, { status: 200 })
+}
