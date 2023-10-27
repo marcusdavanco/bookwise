ff83e31ead99e8e18c5410189f05dacebd8238e2
Marcus Davanço <md.daves@gmail.com>
2023-10-27 05:43:29 -0300

    style: stars component adjustment

--- src/components/stars.tsx
+++ src/components/stars.tsx
@@ -7,10 +7,10 @@ interface StarsProps {
 export function Stars({ rate }: StarsProps) {
   return (
     <div className="flex gap-1">
-      {Array.from({ length: rate }, (_, index) => index).map((index, i) => (
+      {Array.from({ length: rate }, (_, index) => index).map((index) => (
         <Star key={index} className="h-4 w-4 text-purple-100" fill="#8381D9" />
       ))}
-      {Array.from({ length: 5 - rate }, (_, index) => index).map((index, i) => (
+      {Array.from({ length: 5 - rate }, (_, index) => index).map((index) => (
         <Star key={index} className="h-4 w-4 text-purple-100" />
       ))}
     </div>
